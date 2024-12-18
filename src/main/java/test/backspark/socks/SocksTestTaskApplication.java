e5f3a42e3ad2d6e34c74e9a147973e3813ae66dd
Phyllomedusa <matthew.vladimirovich@gmail.com>
2024-12-18 19:59:58 +0300

    Initial commit

---
+++ src/main/java/test/backspark/socks/SocksTestTaskApplication.java
@@ -0,0 +1,13 @@
+package test.backspark.socks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/backspark/socks/SocksTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package test.backspark.socks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
